2ee25e3f086a5524e029f5f3a2f54b4e2fa0aefe
radik93 <ra.abdullin@yandex.ru>
2022-05-06 00:31:05 +0300

    2

--- jasonxml/src/main/java/ru/inovus/jasonxml/utils/FileWrite.java
+++ jasonxml/src/main/java/ru/inovus/jasonxml/utils/FileWrite.java
@@ -2,24 +2,54 @@ package ru.inovus.jasonxml.utils;
 
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
-public class FileWrite {
+
+public class FileWriteImpl {
 
     private static final String fileURL = "out/" + "jsonf" + ".json";
 
     private static final File file= new File(fileURL);
 
+
     //максимальный размер файла в байтах который можно вывести на ЭФ
     private static final long allowableLengthFile = 10000;
 
-    public static void writeJSONFile(String temp) {
+
+    public static void writeFile(String temp,long lineNumber)
+    {
         try {
-            FileWriter fw = new FileWriter(file, true);
-            fw.write(temp, 0, temp.length());
-            fw.close();
+            FileChannel channel=FileChannel.open(Paths.get(fileURL), StandardOpenOption.READ,StandardOpenOption.WRITE);
+            ByteBuffer buff = ByteBuffer.wrap(temp.getBytes(StandardCharsets.UTF_8));
+            channel.write(buff,lineNumber);
+            channel.close();
+                   } catch (IOException e) {
+                       e.printStackTrace();
+                   }
+    }
+
+    public static void addEmpty(){
+        String emptyText;
+        emptyText="                                           "
+                +"                                            \n";
+        try {
+        FileWriter fw = new FileWriter(file, true);
+        fw.write(emptyText, 0, emptyText.length());
+        fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static long getSizeFile()
+    {
+
+        return file.length();
     }
 
     public static void cleanFile() {
@@ -50,12 +80,13 @@ public class FileWrite {
                 BufferedReader reader = new BufferedReader(new FileReader(fileURL));
                 String line = null;
                 StringBuilder stringBuilder = new StringBuilder();
-                String ls = System.getProperty("line.separator");
+             //   String ls = System.getProperty("line.separator");
                 while ((line = reader.readLine()) != null) {
                     stringBuilder.append(line);
-                    stringBuilder.append(ls);
-                    return stringBuilder.toString();
+                 //   stringBuilder.append(ls);
                 }
+                    return stringBuilder.toString();
+
             } catch (IOException e) {
                 return "Error read file";
             }
